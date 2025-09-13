1beaad06773d7e276adba4c2de7915b126d5cf93
syamalasudha <syamalasudha139@gmail.com>
2025-09-13 10:23:45 +0530

    react fundamentals

--- practice/src/ProductCard.jsx
+++ practice/src/ProductCard.jsx
@@ -1,11 +1,9 @@
-function ProductCard({Productname,Productprice,isavailable}){
-    
-    
+function ProductCard(Product){
     return (
         <div>
-            <p>Productname:{Productname}</p>
-            <p>Productprice:{Productprice}</p>
-            <p>{isavailable===true?"In Stock":"Out of stock"}</p>
+            <p>Productname:{Product.Productname}</p>
+            <p>Productprice:{Product.Productprice}</p>
+            <p>{Product.isavailable===true?"In Stock":"Out of stock"}</p>
             
         </div>
 
